002621a79a69d6186528e92c82671da02ff1be19
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-07-15 11:32:41 +0300

    Пробный коммит

---
+++ src/main/java/by/grsu/example/Application.java
@@ -0,0 +1,10 @@
+package by.grsu.example;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");// TODO Auto-generated method stub
+
+	}
+
+}
