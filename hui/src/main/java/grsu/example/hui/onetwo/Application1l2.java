500d81d3c0e6b3b0cf098df788e05409764b3c3b
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-07-22 07:53:54 +0300

    Задание 1:2

---
+++ hui/src/main/java/grsu/example/hui/onetwo/Application1l2.java
@@ -0,0 +1,10 @@
+package grsu.example.hui.onetwo;
+
+public class Application1l2 {
+
+	public static void main(String[] args) {
+		System.out.println("C:\\jdk\\jdk1.8.0_102\\bin\\java.exe");	// TODO Auto-generated method stub
+
+	}
+
+}
