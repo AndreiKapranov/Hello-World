7a7d1d4089b73e77bdcb63ea9a80eccc42942f24
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-07-24 10:09:17 +0300

    1\4

---
+++ hui/src/main/java/grsu/example/hui/onefour/Application1I4.java
@@ -0,0 +1,19 @@
+package grsu.example.hui.onefour;
+
+public class Application1I4 {
+
+	public static void main(String[] args) {
+	byte d = 127;
+	d+=1;//d=-128
+	System.out.println(d);// TODO Auto-generated method stub
+    byte c = 127;
+    c = ++c;//c=-128
+    
+     
+    
+ 
+    System.out.println(c);
+	}
+
+}
+
