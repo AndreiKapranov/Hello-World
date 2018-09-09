e8952ba14f6bb45ee649771b9122226b5f90acff
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-09-09 22:43:39 +0300

    h

--- hui/src/main/java/grsu/example/hui/onethree/Application1l3.java
+++ hui/src/main/java/grsu/example/hui/onethree/Application1l3.java
@@ -3,17 +3,17 @@ package grsu.example.hui.onethree;
 public class Application1l3 {
 
 	public static void main(String[] args) {
-	final byte F_F = 8;
-	short g = 10;
-	double n = 123.4567;
-	char c = 'R';
-	boolean b = true;
-	System.out.println("constant "+F_F);
-	System.out.println("count "+g);
-	System.out.println("price "+n);
-	System.out.println("character \""+c+"\"");
-	System.out.println("active "+b);
-// TODO Auto-generated method stub
+		final byte F_F = 8;
+		short g = 10;
+		double n = 123.4567;
+		char c = 'R';
+		boolean b = true;
+		System.out.println("constant " + F_F);
+		System.out.println("count " + g);
+		System.out.println("price " + n);
+		System.out.println("character \"" + c + "\"");
+		System.out.println("active " + b);
+		// TODO Auto-generated method stub
 
 	}
 
