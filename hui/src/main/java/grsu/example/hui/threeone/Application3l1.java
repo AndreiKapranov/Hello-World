642ecdf08c4ef84abab71e932a535c46f63131eb
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-09-09 23:25:16 +0300

    Наверное лучше было использовать double,нежели int.

--- hui/src/main/java/grsu/example/hui/threeone/Application3l1.java
+++ hui/src/main/java/grsu/example/hui/threeone/Application3l1.java
@@ -5,7 +5,7 @@ public class Application3l1 {
 	public static void main(String[] args) {
 		int a = 5;
 		int b = 6;
-		char operation = '*';
+		char operation = '+';
 		System.out.println("Первое число a=" + a);
 		System.out.println("Второе число b=" + b);
 		if (operation == '+') {
