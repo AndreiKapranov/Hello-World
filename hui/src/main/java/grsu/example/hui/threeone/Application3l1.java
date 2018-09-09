a9743c1032a45886f7b51e40d6e4cdef3807d827
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-09-09 22:24:33 +0300

    Вместо int видимо следовало взять double?

---
+++ hui/src/main/java/grsu/example/hui/threeone/Application3l1.java
@@ -0,0 +1,31 @@
+package grsu.example.hui.threeone;
+
+public class Application3l1 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		char operation = '*';
+		System.out.println("Первое число a=" + a);
+		System.out.println("Второе число b=" + b);
+		if (operation == '+') {
+			int c = a + b;
+			System.out.println("Операция -> +");
+			System.out.println(a + "+" + b + "=" + c);
+		}
+		if (operation == '-') {
+			int c = a - b;
+			System.out.println("Операция -> -");
+			System.out.println(a + "-" + b + "=" + c);}
+			if (operation == '*') {
+				int c = a * b;
+				System.out.println("Операция -> *");
+				System.out.println(a + "*" + b + "=" + c);}
+			if (operation == '/') {
+				int c = a / b;
+				System.out.println("Операция -> /");
+				System.out.println(a + "/" + b + "=" + c);}
+
+		}
+	}
+
