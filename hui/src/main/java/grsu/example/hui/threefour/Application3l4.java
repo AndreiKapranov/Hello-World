bd7c5e8ff13a6224c7b706aa228d3459dbe3f438
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-11-12 13:24:01 +0300

    3.4

--- hui/src/main/java/grsu/example/hui/threefour/Application3l4.java
+++ hui/src/main/java/grsu/example/hui/threefour/Application3l4.java
@@ -7,47 +7,13 @@ public class Application3l4 {
 		System.out.println("„исло  вадрат");
 		System.out.println("-------------");
 		int number = 1;
-		
+
 		while (number < 11) {
-			
+
 			double result = Math.pow(number, 2);
-			switch (number) {
-
-			case 1:
-				System.out.println(number + "       " + result);
-				break;
-			case 2:
-				System.out.println(number + "       " + result);
-				break;
-			case 3:
-				System.out.println(number + "       " + result);
-				break;
-			case 4:
-				System.out.println(number + "       " + result);
-				break;
-			case 5:
-				System.out.println(number + "       " + result);
-				break;
-			case 6:
-				System.out.println(number + "       " + result);
-				break;
-			case 7:
-				System.out.println(number + "       " + result);
-				break;
-			case 8:
-				System.out.println(number + "       " + result);
-				break;
-			case 9:
-				System.out.println(number + "       " + result);
-				break;
-			case 10:
-				System.out.println(number + "      " + result);
-				break;}
-				number ++ ;
-
-			
+			System.out.println(number + "       " + result);
+			number++;
 		}
-
 		System.out.println("-------------");
 
 	}
