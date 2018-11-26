ed3a96b81f508f83921f045e46e7490b79bbf00e
AndreiKapranov <andrej.kapranov.85@mail.ru>
2018-11-26 11:11:54 +0300

    4.1

--- hui/src/main/java/grsu/example/hui/fourone/Application4l1.java
+++ hui/src/main/java/grsu/example/hui/fourone/Application4l1.java
@@ -3,58 +3,102 @@ package grsu.example.hui.fourone;
 import java.util.Scanner;
 
 public class Application4l1 {
+
 	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter array length: ");
-		int size = input.nextInt();
-		if (size > 10) {
-			System.out.println("Error");
-		}
-		double array[] = new double[size]; 
-		System.out.println("Insert array elements:");
-		
+		int length = 0;
+		do {
+			System.out.println("Enter array length: ");
+			length = input.nextInt();
 
-		for (int i = 0; i < size; i++) {
-			array[i] = input.nextDouble(); 
+			if (length <= 0 || length > 10) {
+				System.out.println("длина не может быть больше 10, отрицательным числом или нулём");
+			}
+		} while (length <= 0 || length > 10);
+
+		double array[] = new double[length];
+
+		for (int i = 0; i < length; i++) {
+			System.out.println("Insert array element #" + (i + 1) + ":");
+
+			array[i] = input.nextDouble();
 		}
+
 		input.close();
+
 		double sum = 0;
-		for (int i = 0; i < size; i++) {
+
+		for (int i = 0; i < length; i++) {
+
 			if (array[i] < 0)
+
 				sum += array[i];
+
 		}
+
 		System.out.println("Sum:" + sum);
-		
+
 		double mi = array[0];
+
 		double ma = mi;
+
 		int index_mi = 0;
+
 		int index_ma = 0;
+
 		int x;
-		for (x = 0; x < array.length; x++) {
+
+		for (x = 1; x < array.length; x++) {
+
 			if (array[x] < mi) {
+
 				mi = array[x];
+
 				index_mi = x;
+
 			}
+
 			if (array[x] > ma) {
+
 				ma = array[x];
+
 				index_ma = x;
+
 			}
-		} 
+
+		}
+
 		double result = 1.0;
+		if (index_mi == (index_ma + 1) || index_mi == (index_ma - 1)) {// чтоб в случае когда mi и ma стоят рядом-не
+																		// было Product: 1.0
+			result = 0;
+		}
+
 		if (index_mi > index_ma) {
-			
+
 			int y;
-			for (y = (index_mi - 1); y != index_ma; y--) {
+
+			for (y = (index_mi - 1); y > index_ma; y--) {
 
 				result = result * array[y];
+
 			}
+
 		} else {
-			
+
 			int y;
-			for (y = (index_mi + 1); y != index_ma; y++) {
+
+			for (y = (index_mi + 1); y < index_ma; y++) {
+
 				result = result * array[y];
+
 			}
+
 		}
+
 		System.out.println("Product:" + result);
+
 	}
+
 }
