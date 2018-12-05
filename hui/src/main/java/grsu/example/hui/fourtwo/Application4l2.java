00c5f99a85e943a63278e184d0961e6d22e347ef
Family <Family@192.168.100.62>
2018-12-05 10:59:29 +0200

    4.2

--- hui/src/main/java/grsu/example/hui/fourtwo/Application4l2.java
+++ hui/src/main/java/grsu/example/hui/fourtwo/Application4l2.java
@@ -1,5 +1,6 @@
 package grsu.example.hui.fourtwo;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application4l2 {
@@ -18,16 +19,17 @@ public class Application4l2 {
 
 		input.close();
 		System.out.println("Массив:");
-		Double[] elements = new Double[length];
+		double[] elements = new double[length];
+		Random random = new Random();
 		int i = 0;
 		for (i = 0; i < length; i++) {
 
-			elements[i] = (double) Math.floor(Math.random() * ((7 - (-5)) + 1)) + (-5);
+			elements[i] = random.nextDouble() * 13.0 - 5.0;
 			System.out.println(elements[i]);
 		}
 		System.out.println("Изменённый массив:");
 		for (i = 0; i < length; i++) {
-			if (elements[i] <= 1 && elements[i] >= -1) {
+			if (Math.abs(elements[i]) <= 1) {
 				elements[i] = 0.0;
 			}
 			System.out.println(elements[i]);
