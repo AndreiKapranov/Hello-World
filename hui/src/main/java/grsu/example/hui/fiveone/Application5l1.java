6e6f61669cdc006c8e25f66f6567ccf5c05b97e5
AndreiKapranov <andrej.kapranov.85@mail.ru>
2019-03-28 22:34:27 +0300

    5.1

---
+++ hui/src/main/java/grsu/example/hui/fiveone/Application5l1.java
@@ -0,0 +1,34 @@
+package grsu.example.hui.fiveone;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+import java.util.Scanner;
+
+public class Application5l1 {
+
+	double x = 0.0;
+	double y = 0.0;
+	double x1 = 0.0;
+	double y1 = 0.0;
+
+	static double distance(double x, double y, double x1, double y1) {
+		return sqrt(pow(x1 - x, 2) + pow(y1 - y, 2));
+	}
+
+	public static void main(String... args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter coordinate x > ");
+		double x = scanner.nextDouble();
+		System.out.print("Enter coordinate y > ");
+		double y = scanner.nextDouble();
+		System.out.print("Enter coordinate x1 > ");
+		double x1 = scanner.nextDouble();
+		System.out.print("Enter coordinate y1 > ");
+		double y1 = scanner.nextDouble();
+		scanner.close();
+
+		System.out.println("Distance between two points:" + distance(x, y, x1, y1));
+
+	}
+}
+
