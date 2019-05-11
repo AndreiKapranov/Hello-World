72d6fee039088daa98e56da29cbf3f3b1072cf5f
AndreiKapranov <andrej.kapranov.85@mail.ru>
2019-05-11 18:19:13 +0300

    5.2

--- hui/src/main/java/grsu/example/hui/fivetwo/Application5l2.java
+++ hui/src/main/java/grsu/example/hui/fivetwo/Application5l2.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Application5l2 {
 	public static int gain(int x) {
-		int a = (x - (x % 100)) / 100;
+		int a = x / 100;
 		int c = x % 10;
-		int b = ((x % 100) - c)/10;
+		int b = (x % 100)/10;
 		x = c * 100 + b * 10 + a;
 
 		return x;
